decade3fabbfe365c977a283469c5725c665bb09
unknown <greveson.axel@gmail.com>
2024-10-12 19:51:48 +0200

    stats reworked

--- src/components/StatSlot/StatSlot.tsx
+++ src/components/StatSlot/StatSlot.tsx
@@ -1,5 +1,5 @@
 import { StatTypes } from "../../models/StatTypes";
-import {statTranslations} from "../../utils/translations/StatTypes";
+import { statTranslations } from "../../utils/translations/StatTypes";
 
 type Props = {
   statType: StatTypes;
@@ -9,15 +9,17 @@ type Props = {
  * @param statType Indiquer s'il s'agit de "strength", etc.
  */
 const StatSlot = ({ statType }: Props) => {
-
   /**
    * statType = strength, constitution, charisma, etc
    * statTranslations[statType] = Force, Constitution, Charisme, etc
    */
 
   return (
-    <div className={`static-stat ${statType}`}>
-      {statTranslations[statType]}
+    <div className="static-stat">
+      <div className={`static-stat-name ${statType}`}>
+        {statTranslations[statType]}
+      </div>
+      <div className="static-stat-value">50</div>
     </div>
   );
 };
